f416c5760c6fe51827c763d4d2e20f5d80d480dc
Aiden McClelland <me@drbonez.dev>
2021-09-11 19:34:53 -0600

    fix

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -e
 
 echo 'Building Container'
 docker build . -t start9/rust-arm-cross:aarch64
-echo << EOF
+echo << EOF > /usr/local/bin/rust-arm64-builder
 #!/bin/bash
-docker run --rm -it -v "$HOME/.cargo/registry":/root/.cargo/registry -v $(pwd):/home/rust/src start9/rust-arm-cross:aarch64 $@" > /usr/local/bin/rust-arm64-builder
+docker run --rm -it -v "$HOME/.cargo/registry":/root/.cargo/registry -v $(pwd):/home/rust/src start9/rust-arm-cross:aarch64 $@
 EOF
 chmod +x /usr/local/bin/rust-arm64-builder
