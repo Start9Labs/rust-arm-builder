1079e733a6a363b3cb9385244f21fbbfeae9fbea
Aiden McClelland <me@drbonez.dev>
2021-12-14 17:33:44 -0700

    sudo for command installation

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -e
 
 echo 'Building Container'
 docker build . -t start9/rust-arm-cross:aarch64
-echo << EOF > /usr/local/bin/rust-arm64-builder
+echo << EOF | sudo tee /usr/local/bin/rust-arm64-builder
 #!/bin/bash
 docker run --rm -it -v "$HOME/.cargo/registry":/root/.cargo/registry -v $(pwd):/home/rust/src start9/rust-arm-cross:aarch64 $@
 EOF
-chmod +x /usr/local/bin/rust-arm64-builder
+sudo chmod +x /usr/local/bin/rust-arm64-builder
