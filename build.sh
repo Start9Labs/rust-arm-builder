2264dbee95bf774a5988d5da869c9736a79dce29
Aiden McClelland <me@drbonez.dev>
2023-03-08 18:02:37 -0700

    use cat instead of echo

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 echo 'Building Container'
 docker build . -t start9/rust-arm-cross:aarch64
-echo << EOF | sudo tee /usr/local/bin/rust-arm64-builder
+cat << 'EOF' | sudo tee /usr/local/bin/rust-arm64-builder
 #!/bin/bash
 docker run --rm -it -v "$HOME/.cargo/registry":/root/.cargo/registry -v $(pwd):/home/rust/src start9/rust-arm-cross:aarch64 $@
 EOF
